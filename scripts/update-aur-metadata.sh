ad80b46be06b6a0f35e237ee373be9dbb924bd9a
OneNoted <notes@madeingotland.com>
2026-04-14 09:57:21 +0200

    fix: avoid expired nobody account in CI

--- scripts/update-aur-metadata.sh
+++ scripts/update-aur-metadata.sh
@@ -10,10 +10,19 @@ checksum=$(sha256sum "$asset" | awk '{print $1}')
 
 perl -0pi -e "s/^pkgver=.*/pkgver=${pkgver}/m; s/^pkgrel=.*/pkgrel=${pkgrel}/m; s/^_libggml_pkgver=.*/_libggml_pkgver=${libggml_pkgver}/m; s/^_libggml_pkgrel=.*/_libggml_pkgrel=${libggml_pkgrel}/m; s/^sha256sums=\('[^']*'\)/sha256sums=('${checksum}')/m" PKGBUILD
 
-if [[ ${EUID} -eq 0 ]]; then
-  su nobody -s /bin/sh -c 'makepkg --printsrcinfo' > .SRCINFO
-else
-  makepkg --printsrcinfo > .SRCINFO
-fi
+print_srcinfo() {
+  if [[ ${EUID} -ne 0 ]]; then
+    makepkg --printsrcinfo
+    return
+  fi
+
+  local temp_home
+  temp_home=$(mktemp -d)
+  trap 'rm -rf "$temp_home"' RETURN
+  HOME="$temp_home" setpriv --reuid 65534 --regid 65534 --clear-groups \
+    makepkg --printsrcinfo
+}
+
+print_srcinfo > .SRCINFO
 
 printf 'Updated PKGBUILD and .SRCINFO for %s-%s\n' "$pkgver" "$pkgrel"
--- scripts/validate-repo.sh
+++ scripts/validate-repo.sh
@@ -6,10 +6,19 @@ bash -n PKGBUILD scripts/*.sh
 generated=$(mktemp)
 trap 'rm -f "$generated"' EXIT
 
-if [[ ${EUID} -eq 0 ]]; then
-  su nobody -s /bin/sh -c 'makepkg --printsrcinfo' > "$generated"
-else
-  makepkg --printsrcinfo > "$generated"
-fi
+print_srcinfo() {
+  if [[ ${EUID} -ne 0 ]]; then
+    makepkg --printsrcinfo
+    return
+  fi
+
+  local temp_home
+  temp_home=$(mktemp -d)
+  trap 'rm -rf "$temp_home"' RETURN
+  HOME="$temp_home" setpriv --reuid 65534 --regid 65534 --clear-groups \
+    makepkg --printsrcinfo
+}
+
+print_srcinfo > "$generated"
 
 diff -u .SRCINFO "$generated"
